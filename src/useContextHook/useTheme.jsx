e6e8e13854f371c139c848ced3cf8da05680f880
surajkr20 <suraj71308kumar@gmail.com>
2025-03-21 07:11:57 +0530

    theme context setup

---
+++ src/useContextHook/useTheme.jsx
@@ -0,0 +1,19 @@
+ 
+import React,{useContext, createContext, useState} from 'react';
+
+const ThemeContexts = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTheme = () => useContext(ThemeContexts);
+
+export const ThemeProvider = ((children) =>{
+    const [isDarkMode, setIsDarkMode] = useState();
+    const toggleTheme = () =>{
+        setIsDarkMode(prevMode => !prevMode);
+    }
+    return (
+        <ThemeContexts.Provider value={{toggleTheme, isDarkMode}}>
+            {children}
+        </ThemeContexts.Provider>
+    )
+})
